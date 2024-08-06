916c7e12ab21b0b22e3659437bfddc6920c91316
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2024-08-06 22:22:18 +0530

    PHP with HTML

---
+++ php-with-html.php
@@ -0,0 +1,18 @@
+<?php
+$color = "skyblue";
+echo "<h1 style = color:blue;> php with html </h1>";
+echo "<h1 style = color:blue;> php with html </h1>";
+?>
+
+<?php
+$name = "Ashish Kukreti";
+$name1 = "op";
+echo "<h3> My name is $name </h3>";
+// echo phpinfo();
+?>
+
+<h1 style=color:green>
+    My name is <?php echo $name; ?>
+</h1>
+
+<h1 style = "color: <?php echo $color ?>" > my name is <?php echo $name1 ?> </h1>

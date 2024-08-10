360d102d23c7cb47b94f9f133cdd80ef127694a7
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2024-08-10 17:56:05 +0530

    Datatypes in PHP

---
+++ datatypes.php
@@ -0,0 +1,21 @@
+<?php
+$name = "Ashihs Kukreti";
+echo var_dump($name);
+echo "</br>";
+$num = 10;
+echo var_dump($num);
+echo "</br>";
+$float = 10.01;
+echo var_dump($float);
+echo "</br>";
+$bool = true;
+echo var_dump($bool);
+echo "</br>";
+$arr = ["ashish","OP",10,10.1,false];
+echo var_dump($arr);
+echo "</br";
+$empty=null;
+echo var_dump($empty);
+echo "</br>";
+
+?>

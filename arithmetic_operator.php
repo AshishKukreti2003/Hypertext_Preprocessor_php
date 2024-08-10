65e2bb0309212f27338a207eb2868b9be160861f
AshishKukreti2003 <ashishkukreti4546@gmail.com>
2024-08-10 17:56:47 +0530

    Operators in PHP

---
+++ arithmetic_operator.php
@@ -0,0 +1,16 @@
+<?php
+$a = 10;
+$b = 8;
+echo $a+$b;
+echo "</br>";
+echo $a-$b;
+echo "</br>";
+echo $a*$b;
+echo "</br>";
+echo $a/$b;
+echo "</br>";
+echo $a%$b;
+echo "</br>";
+echo $a**$b;
+echo "</br>";
+?>
